9d408155e8ca13867c52175e61e9d93f43ac9123
Jannik Glückert <jannik.glueckert@gmail.com>
2024-01-13 20:22:59 +0100

    bindings: use py namespace alias
    Signed-off-by: Jannik Glückert <jannik.glueckert@gmail.com>

--- bindings/python/atom/atom.cpp
+++ bindings/python/atom/atom.cpp
@@ -3,12 +3,13 @@
 #include "../common.hpp"
 #include "atom/atom.hpp"
 #include "atom/atom_parser.hpp"
-#include "depend/depend_parser.hpp"
 
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <string_view>
+
+namespace py = pybind11;
 
 using namespace pms_utils::atom;
 
@@ -20,15 +21,15 @@ template <typename T> struct type_caster<boost::optional<T>> : optional_caster<b
 
 namespace pms_utils::bindings::python::atom {
 
-void _register(pybind11::module &_module) {
+void _register(py::module &_module) {
     // boy the lack of reflection is my favourite part of C++
 
-    pybind11::module atom = _module.def_submodule("atom");
+    py::module atom = _module.def_submodule("atom");
 
     // TODO
     // Version number components should be exposed as ints as python uses VarInts
 
-    pybind11::enum_<VersionSpecifier>(atom, "VersionSpecifier")
+    py::enum_<VersionSpecifier>(atom, "VersionSpecifier")
         .value("lt", VersionSpecifier::lt)
         .value("le", VersionSpecifier::le)
         .value("eq", VersionSpecifier::eq)
@@ -36,100 +37,97 @@ void _register(pybind11::module &_module) {
         .value("td", VersionSpecifier::td)
         .value("ge", VersionSpecifier::ge)
         .value("gt", VersionSpecifier::gt)
-        .def(pybind11::init(
-            [](std::string_view str) { return expr_from_str(parsers::version_specifier, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::version_specifier, str); }))
         .def(
-            "__repr__", [](VersionSpecifier verspec) { return to_string(verspec); },
-            pybind11::name("__repr__"), pybind11::is_method(atom));
+            "__repr__", [](VersionSpecifier verspec) { return to_string(verspec); }, py::name("__repr__"),
+            py::is_method(atom));
 
-    pybind11::enum_<Blocker>(atom, "Blocker")
+    py::enum_<Blocker>(atom, "Blocker")
         .value("weak", Blocker::weak)
         .value("strong", Blocker::strong)
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::blocker, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::blocker, str); }))
         .def(
-            "__repr__", [](Blocker blocker) { return to_string(blocker); }, pybind11::name("__repr__"),
-            pybind11::is_method(atom));
+            "__repr__", [](Blocker blocker) { return to_string(blocker); }, py::name("__repr__"),
+            py::is_method(atom));
 
-    pybind11::enum_<VersionSuffixWord>(atom, "VersionSuffixWord")
+    py::enum_<VersionSuffixWord>(atom, "VersionSuffixWord")
         .value("alpha", VersionSuffixWord::alpha)
         .value("beta", VersionSuffixWord::beta)
         .value("pre", VersionSuffixWord::pre)
         .value("rc", VersionSuffixWord::rc)
         .value("p", VersionSuffixWord::p)
-        .def(pybind11::self < pybind11::self)
-        .def(pybind11::self <= pybind11::self)
-        .def(pybind11::self > pybind11::self)
-        .def(pybind11::self >= pybind11::self)
-        .def(pybind11::self == pybind11::self)
-        .def(pybind11::self != pybind11::self)
+        .def(py::self < py::self)
+        .def(py::self <= py::self)
+        .def(py::self > py::self)
+        .def(py::self >= py::self)
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix_word, str); }))
         .def(
-            pybind11::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix_word, str); }))
-        .def(
-            "__repr__", [](VersionSuffixWord word) { return to_string(word); }, pybind11::name("__repr__"),
-            pybind11::is_method(atom));
+            "__repr__", [](VersionSuffixWord word) { return to_string(word); }, py::name("__repr__"),
+            py::is_method(atom));
 
-    pybind11::class_<VersionSuffix>(atom, "VersionSuffix")
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix, str); }))
+    py::class_<VersionSuffix>(atom, "VersionSuffix")
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix, str); }))
         .def_readonly("word", &VersionSuffix::word)
         .def_readonly("number", &VersionSuffix::number)
         .def("__repr__", [](const VersionSuffix &suffix) { return std::string(suffix); });
 
-    pybind11::class_<Version>(atom, "Version")
-        .def(
-            pybind11::init([](std::string_view str) { return expr_from_str(parsers::package_version, str); }))
+    py::class_<Version>(atom, "Version")
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::package_version, str); }))
         .def_readonly("numbers", &Version::numbers)
         .def_readonly("letter", &Version::letter)
         .def_readonly("suffixes", &Version::suffixes)
         .def_readonly("revision", &Version::revision)
-        .def(pybind11::self < pybind11::self)
-        .def(pybind11::self <= pybind11::self)
-        .def(pybind11::self > pybind11::self)
-        .def(pybind11::self >= pybind11::self)
-        .def(pybind11::self == pybind11::self)
-        .def(pybind11::self != pybind11::self)
+        .def(py::self < py::self)
+        .def(py::self <= py::self)
+        .def(py::self > py::self)
+        .def(py::self >= py::self)
+        .def(py::self == py::self)
+        .def(py::self != py::self)
         .def("__repr__", [](const Version &version) { return std::string(version); });
 
-    pybind11::class_<Slot>(atom, "Slot")
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::slot, str); }))
+    py::class_<Slot>(atom, "Slot")
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::slot, str); }))
         .def_readonly("slot", &Slot::slot)
         .def_readonly("subslot", &Slot::subslot)
         .def("__repr__", [](const Slot &slot) { return std::string(slot); });
 
-    pybind11::enum_<SlotVariant>(atom, "SlotVariant")
+    py::enum_<SlotVariant>(atom, "SlotVariant")
         .value("none", SlotVariant::none)
         .value("star", SlotVariant::star)
         .value("equal", SlotVariant::equal);
 
-    pybind11::class_<SlotExpr>(atom, "SlotExpr")
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::slot_expr, str); }))
+    py::class_<SlotExpr>(atom, "SlotExpr")
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::slot_expr, str); }))
         .def_readonly("slot_variant", &SlotExpr::slotVariant)
         .def_readonly("slot", &SlotExpr::slot)
         .def("__repr__", [](const SlotExpr &slot) { return std::string(slot); });
 
-    pybind11::enum_<UsedepSign>(atom, "UsedepSign")
+    py::enum_<UsedepSign>(atom, "UsedepSign")
         .value("plus", UsedepSign::plus)
         .value("minus", UsedepSign::minus)
         .def(
-            "__repr__", [](UsedepSign sign) { return to_string(sign); }, pybind11::name("__repr__"),
-            pybind11::is_method(atom));
+            "__repr__", [](UsedepSign sign) { return to_string(sign); }, py::name("__repr__"),
+            py::is_method(atom));
 
-    pybind11::enum_<UsedepCond>(atom, "UsedepCond")
+    py::enum_<UsedepCond>(atom, "UsedepCond")
         .value("equal", UsedepCond::eqal)
         .value("question", UsedepCond::question)
         .def(
-            "__repr__", [](UsedepCond cond) { return to_string(cond); }, pybind11::name("__repr__"),
-            pybind11::is_method(atom));
+            "__repr__", [](UsedepCond cond) { return to_string(cond); }, py::name("__repr__"),
+            py::is_method(atom));
 
-    pybind11::class_<Usedep>(atom, "Usedep")
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::use_dep, str); }))
+    py::class_<Usedep>(atom, "Usedep")
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::use_dep, str); }))
         .def_readonly("negate", &Usedep::negate)
         .def_readonly("useflag", &Usedep::useflag)
         .def_readonly("sign", &Usedep::sign)
         .def_readonly("conditional", &Usedep::conditional)
         .def("__repr__", [](const Usedep &usedep) { return std::string(usedep); });
 
-    pybind11::class_<PackageExpr>(atom, "Atom")
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::atom, str); }))
+    py::class_<PackageExpr>(atom, "Atom")
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::atom, str); }))
         .def_readonly("blocker", &PackageExpr::blocker)
         .def_readonly("category", &PackageExpr::category)
         .def_readonly("name", &PackageExpr::name)
--- bindings/python/depend/depend.cpp
+++ bindings/python/depend/depend.cpp
@@ -9,6 +9,8 @@
 #include <pybind11/stl.h>
 #include <string_view>
 
+namespace py = pybind11;
+
 using namespace pms_utils::depend;
 
 namespace PYBIND11_NAMESPACE {
@@ -30,31 +32,30 @@ template <> struct visit_helper<boost::variant> {
 
 namespace pms_utils::bindings::python::depend {
 
-void _register(pybind11::module &_module) {
-    pybind11::module depend = _module.def_submodule("depend");
+void _register(py::module &_module) {
+    py::module depend = _module.def_submodule("depend");
 
-    pybind11::class_<UseConditional>(depend, "UseConditional")
+    py::class_<UseConditional>(depend, "UseConditional")
         .def_readonly("negate", &UseConditional::negate)
         .def_readonly("useflag", &UseConditional::useflag)
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::use_cond, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::use_cond, str); }))
         .def("__repr__", [](const UseConditional &cond) { return std::string(cond); });
 
-    pybind11::enum_<GroupHeaderOp>(depend, "GroupHeaderOp")
+    py::enum_<GroupHeaderOp>(depend, "GroupHeaderOp")
         .value("any_of", GroupHeaderOp::any_of)
         .value("at_most_one_of", GroupHeaderOp::at_most_one_of)
         .value("exactly_one_of", GroupHeaderOp::exactly_one_of)
         .def(
-            "__repr__", [](GroupHeaderOp header) { return to_string(header); }, pybind11::name("__repr__"),
-            pybind11::is_method(depend));
+            "__repr__", [](GroupHeaderOp header) { return to_string(header); }, py::name("__repr__"),
+            py::is_method(depend));
 
-    pybind11::class_<GroupExpr>(depend, "GroupExpr")
+    py::class_<GroupExpr>(depend, "GroupExpr")
         .def(
-            "__iter__",
-            [](const GroupExpr &expr) { return pybind11::make_iterator(expr.begin(), expr.end()); },
-            pybind11::keep_alive<0, 1>())
+            "__iter__", [](const GroupExpr &expr) { return py::make_iterator(expr.begin(), expr.end()); },
+            py::keep_alive<0, 1>())
         .def_readonly("conditional", &GroupExpr::conditional)
         .def_readonly("nodes", &GroupExpr::nodes)
-        .def(pybind11::init([](std::string_view str) { return expr_from_str(parsers::group, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::group, str); }))
         .def("__repr__", [](const GroupExpr &expr) { return std::string(expr); });
 
     depend.def("DependExpr", [](std::string_view str) { return expr_from_str(parsers::nodes, str); });
