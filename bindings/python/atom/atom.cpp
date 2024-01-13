b755d6ba31ef3d24d3a839d5b017f7b37e1b0b68
Jannik Glückert <jannik.glueckert@gmail.com>
2024-01-13 20:54:21 +0100

    bindings: drop GIL in parser calls
    Signed-off-by: Jannik Glückert <jannik.glueckert@gmail.com>

--- bindings/python/atom/atom.cpp
+++ bindings/python/atom/atom.cpp
@@ -37,7 +37,8 @@ void _register(py::module &_module) {
         .value("td", VersionSpecifier::td)
         .value("ge", VersionSpecifier::ge)
         .value("gt", VersionSpecifier::gt)
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::version_specifier, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::version_specifier, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def(
             "__repr__", [](VersionSpecifier verspec) { return to_string(verspec); }, py::name("__repr__"),
             py::is_method(atom));
@@ -45,7 +46,8 @@ void _register(py::module &_module) {
     py::enum_<Blocker>(atom, "Blocker")
         .value("weak", Blocker::weak)
         .value("strong", Blocker::strong)
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::blocker, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::blocker, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def(
             "__repr__", [](Blocker blocker) { return to_string(blocker); }, py::name("__repr__"),
             py::is_method(atom));
@@ -62,19 +64,22 @@ void _register(py::module &_module) {
         .def(py::self >= py::self)
         .def(py::self == py::self)
         .def(py::self != py::self)
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix_word, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix_word, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def(
             "__repr__", [](VersionSuffixWord word) { return to_string(word); }, py::name("__repr__"),
             py::is_method(atom));
 
     py::class_<VersionSuffix>(atom, "VersionSuffix")
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::ver_suffix, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def_readonly("word", &VersionSuffix::word)
         .def_readonly("number", &VersionSuffix::number)
         .def("__repr__", [](const VersionSuffix &suffix) { return std::string(suffix); });
 
     py::class_<Version>(atom, "Version")
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::package_version, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::package_version, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def_readonly("numbers", &Version::numbers)
         .def_readonly("letter", &Version::letter)
         .def_readonly("suffixes", &Version::suffixes)
@@ -88,7 +93,8 @@ void _register(py::module &_module) {
         .def("__repr__", [](const Version &version) { return std::string(version); });
 
     py::class_<Slot>(atom, "Slot")
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::slot, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::slot, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def_readonly("slot", &Slot::slot)
         .def_readonly("subslot", &Slot::subslot)
         .def("__repr__", [](const Slot &slot) { return std::string(slot); });
@@ -99,7 +105,8 @@ void _register(py::module &_module) {
         .value("equal", SlotVariant::equal);
 
     py::class_<SlotExpr>(atom, "SlotExpr")
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::slot_expr, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::slot_expr, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def_readonly("slot_variant", &SlotExpr::slotVariant)
         .def_readonly("slot", &SlotExpr::slot)
         .def("__repr__", [](const SlotExpr &slot) { return std::string(slot); });
@@ -119,7 +126,8 @@ void _register(py::module &_module) {
             py::is_method(atom));
 
     py::class_<Usedep>(atom, "Usedep")
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::use_dep, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::use_dep, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def_readonly("negate", &Usedep::negate)
         .def_readonly("useflag", &Usedep::useflag)
         .def_readonly("sign", &Usedep::sign)
@@ -127,7 +135,8 @@ void _register(py::module &_module) {
         .def("__repr__", [](const Usedep &usedep) { return std::string(usedep); });
 
     py::class_<PackageExpr>(atom, "Atom")
-        .def(py::init([](std::string_view str) { return expr_from_str(parsers::atom, str); }))
+        .def(py::init([](std::string_view str) { return expr_from_str(parsers::atom, str); }),
+             py::call_guard<py::gil_scoped_release>())
         .def_readonly("blocker", &PackageExpr::blocker)
         .def_readonly("category", &PackageExpr::category)
         .def_readonly("name", &PackageExpr::name)
