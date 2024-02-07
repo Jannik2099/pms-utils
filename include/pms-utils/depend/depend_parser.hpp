f8ef8b35484a22560ff40d80fd680e1b8f5d33c9
Jannik Glückert <jannik.glueckert@gmail.com>
2024-02-07 18:08:18 +0100

    depend: add generic GroupExpr parser helpers
    Signed-off-by: Jannik Glückert <jannik.glueckert@gmail.com>

--- include/pms-utils/depend/depend_parser.hpp
+++ include/pms-utils/depend/depend_parser.hpp
@@ -13,6 +13,15 @@ namespace x3 = boost::spirit::x3;
 PARSER_RULE_T(use_cond, depend::UseConditional);
 PARSER_RULE_T(conds, depend::GroupHeader);
 
+// myeah this is kinda icky. Suggestions welcome
+template <typename Rule> constexpr static auto GroupTemplate1(Rule rule) {
+    return -(conds() >> x3::omit[+x3::space]) >> x3::lit("(") >> x3::omit[+x3::space] >>
+           rule() % +x3::space >> x3::omit[+x3::space] >> x3::lit(")");
+}
+template <typename Rule> constexpr static auto GroupTemplate2(Rule rule) {
+    return x3::attr(boost::none) >> x3::omit[*x3::space] >> rule() % +x3::space >> x3::omit[*x3::space];
+}
+
 PARSER_RULE_T(group, depend::DependExpr);
 PARSER_RULE_T(node, depend::DependExpr::Node);
 PARSER_RULE_T(nodes, depend::DependExpr);
