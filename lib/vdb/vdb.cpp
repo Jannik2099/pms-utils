e5f546fcf15f681e31b0732eca6fd72caa2e1ea2
John Turner <jturner.usa@gmail.com>
2024-03-23 18:09:13 -0400

    implement Vdb class

--- lib/vdb/vdb.cpp
+++ lib/vdb/vdb.cpp
@@ -1,1 +1,16 @@
+#include "pms-utils/vdb/vdb.hpp"
 
+#include <filesystem>
+
+namespace [[gnu::visibility("default")]] pms_utils {
+namespace vdb {
+
+Vdb::Vdb(std::filesystem::path path) : _path(std::move(path)) {
+    for (const auto &entry : std::filesystem::directory_iterator(_path)) {
+        Category category;
+        categories.push_back(category);
+    }
+}
+
+} // namespace vdb
+} // namespace pms_utils
--- test/vdb/vdb.cpp
+++ test/vdb/vdb.cpp
@@ -1,1 +1,7 @@
-int main(int argc, char **argv) { return 0; }
+#include "pms-utils/vdb/vdb.hpp"
+
+int main(int argc, char **argv) {
+    assert(argc > 1);
+    pms_utils::vdb::Vdb fakevdb(argv[1]);
+    return 0;
+}
