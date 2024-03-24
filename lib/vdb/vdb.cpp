75b1f37ec5cbcb238950098abf8555eb7cbfe0e8
John Turner <jturner.usa@gmail.com>
2024-03-24 15:58:07 -0400

    use ParserResult::display in vdb functions

--- lib/vdb/vdb.cpp
+++ lib/vdb/vdb.cpp
@@ -24,7 +24,7 @@ Category::Category(std::filesystem::path path) : _path(std::move(path)) {
     auto filename = _path.filename().string();
     auto result = pms_utils::try_parse(filename, pms_utils::parsers::category());
     if (result.status != pms_utils::ParserStatus::Success) {
-        throw std::runtime_error(std::format("invalid category in vdb: failed to parse: {}", filename));
+        throw std::runtime_error(std::format("invalid category in vdb: {}", result.display(filename)));
     }
     _category = result.result.value();
     for (const auto &entry : std::filesystem::directory_iterator(_path)) {
@@ -40,13 +40,13 @@ Pkg::Pkg(std::filesystem::path path) : _path(std::move(path)) {
     auto filename = _path.filename().string();
     auto name = pms_utils::try_parse(filename, pms_utils::parsers::name());
     if (name.status != pms_utils::ParserStatus::Progress) {
-        throw std::runtime_error(std::format("invalid package in vdb: failed to parse name: {}", filename));
+        throw std::runtime_error(std::format("invalid package in vdb: {}", name.display(filename)));
     }
     _name = name.result.value();
     auto rest = std::string_view(filename.begin() + name.consumed + 1, filename.end());
     auto version = pms_utils::try_parse(rest, pms_utils::parsers::package_version());
     if (version.status != pms_utils::ParserStatus::Success) {
-        throw std::runtime_error(std::format("invalid package in vdb: failed to parse version: {}", rest));
+        throw std::runtime_error(std::format("invalid package in vdb: {}", version.display(rest)));
     }
     _version = version.result.value();
 }
