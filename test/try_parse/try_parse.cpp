ed2359ef2e196cc0681c500af9807e2a3c73b174
John Turner <jturner.usa@gmail.com>
2024-03-24 15:30:00 -0400

    renamed member from value to result to avoid "result.value.value()"

--- test/try_parse/try_parse.cpp
+++ test/try_parse/try_parse.cpp
@@ -15,23 +15,22 @@ int main(int argc, char **argv) {
     {
         auto category = pms_utils::try_parse(cpv, pms_utils::parsers::category());
         assert(category.status == pms_utils::ParserStatus::Progress);
-        assert(*category.value == "app-editors");
+        assert(*category.result == "app-editors");
         auto rest = std::string_view(cpv.begin() + category.consumed + 1, cpv.end());
-
         auto name = pms_utils::try_parse(rest, pms_utils::parsers::name());
         assert(name.status == pms_utils::ParserStatus::Progress);
-        assert(*name.value == "emacs");
+        assert(*name.result == "emacs");
         rest = std::string_view(rest.begin() + name.consumed + 1, rest.end());
 
         auto version = pms_utils::try_parse(rest, pms_utils::parsers::package_version());
         assert(version.status == pms_utils::ParserStatus::Success);
-        assert(std::string(version.value.value().numbers) == "29.1");
+        assert(std::string(version.result.value().numbers) == "29.1");
     }
 
     {
         auto result = pms_utils::try_parse(atom, pms_utils::parsers::category());
         assert(result.status == pms_utils::ParserStatus::Fail);
-        assert(!result.value.has_value());
+        assert(!result.result.has_value());
         assert(result.consumed == 0);
     }
 }
