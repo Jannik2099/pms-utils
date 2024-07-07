56b12023b30b0c32b5642c7f70daadd1b5525bcd
Jannik Glückert <jannik.glueckert@gmail.com>
2024-07-07 18:05:11 +0200

    bindings: work around missing pybind gcc14 algorithm

--- subprojects/bindings-python/lib/repo/repo.hpp
+++ subprojects/bindings-python/lib/repo/repo.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// https://github.com/pybind/pybind11/commit/51c2aa16de5b50fe4be6a0016d6090d4a831899e
+#include <algorithm> // IWYU pragma: keep
 #include <pybind11/pybind11.h>
 
 namespace pms_utils::bindings::python::repo {
