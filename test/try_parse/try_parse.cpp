7b35c24e3690bac09d54cae0f8b6b0c24ebe176b
John Turner <jturner.usa@gmail.com>
2024-08-27 16:32:27 -0400

    fixup paths in try_parse test

--- test/try_parse/try_parse.cpp
+++ test/try_parse/try_parse.cpp
@@ -8,27 +8,27 @@ int main() {
     const std::string_view cpv = "app-editors/emacs-29.1-r1";
 
     {
-        auto result = pms_utils::try_parse(atom, pms_utils::parsers::atom());
+      auto result = pms_utils::try_parse(atom, pms_utils::parsers::atom::atom());
         assert(result.status == pms_utils::ParserStatus::Success);
     }
 
     {
-        auto category = pms_utils::try_parse(cpv, pms_utils::parsers::category());
+      auto category = pms_utils::try_parse(cpv, pms_utils::parsers::atom::category());
         assert(category.status == pms_utils::ParserStatus::Progress);
         assert(*category.result == "app-editors");
         auto rest = std::string_view(cpv.begin() + category.consumed + 1, cpv.end());
-        auto name = pms_utils::try_parse(rest, pms_utils::parsers::name());
+        auto name = pms_utils::try_parse(rest, pms_utils::parsers::atom::name());
         assert(name.status == pms_utils::ParserStatus::Progress);
         assert(*name.result == "emacs");
         rest = std::string_view(rest.begin() + name.consumed + 1, rest.end());
 
-        auto version = pms_utils::try_parse(rest, pms_utils::parsers::package_version());
+        auto version = pms_utils::try_parse(rest, pms_utils::parsers::atom::package_version());
         assert(version.status == pms_utils::ParserStatus::Success);
         assert(std::string(version.result.value().numbers) == "29.1");
     }
 
     {
-        auto category = pms_utils::try_parse(atom, pms_utils::parsers::category());
+      auto category = pms_utils::try_parse(atom, pms_utils::parsers::atom::category());
         assert(category.status == pms_utils::ParserStatus::Fail);
         assert(!category.result.has_value());
         assert(category.consumed == 0);
