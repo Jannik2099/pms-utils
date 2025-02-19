3f4c0af456e922f3d69d736b9f748cfb235076c2
John Turner <jturner.usa@gmail.com>
2025-02-19 00:05:49 -0500

    add try_parse utility function

---
+++ include/pms-utils/misc/try_parse.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <expected>
+#include <ranges>
+#include <utility>
+
+//
+#include "pms-utils/misc/macro-begin.hpp"
+
+namespace [[gnu::visibility("default")]] pms_utils {
+namespace misc {
+
+template <typename Rule, std::ranges::range Range>
+std::expected<typename Rule::parser_type::attr_type,
+              decltype(std::ranges::cbegin(std::declval<const Range &>()))>
+try_parse(const Range &input [[clang::lifetimebound]], const Rule &rule, bool partial) {
+    typename Rule::parser_type::attr_type ret;
+
+    if (auto iter = std::ranges::cbegin(input);
+        (!prefix_parse(iter, std::ranges::cend(input), rule, ret)) || ((iter != input.end()) && partial)) {
+        return std::unexpected{iter};
+    }
+
+    return ret;
+}
+
+} // namespace misc
+} // namespace pms_utils
+
+//
+#include "pms-utils/misc/macro-end.hpp"
---
+++ test/try_parse/try_parse.cpp
@@ -0,0 +1,59 @@
+#include "pms-utils/misc/try_parse.hpp"
+
+#include "pms-utils/atom/atom.hpp"
+#include "pms-utils/atom/atom_parser.hpp"
+
+#include <cassert>
+#include <iostream>
+#include <ostream>
+#include <string_view>
+
+namespace {
+
+bool test_success() {
+    const std::string_view atom{"=foo/bar-1.0.0"};
+    const auto result = pms_utils::misc::try_parse(atom, pms_utils::parsers::atom::atom, true).value();
+
+    return (result.category == "foo") && (result.name == "bar") && result.version.has_value();
+}
+
+bool test_failure() {
+    const std::string_view atom{"=foo/bar-.0-1"};
+    const auto result = pms_utils::misc::try_parse(atom, pms_utils::parsers::atom::atom, true);
+
+    return std::string_view{result.error(), atom.end()} == ".0-1";
+}
+
+bool test_consume_failure() {
+    const std::string_view bad_atom{"=foo/bar-1.0.0 =foo/bar-1.0.1"};
+    const auto result = pms_utils::misc::try_parse(bad_atom, pms_utils::parsers::atom::atom, true);
+
+    return !result.has_value();
+}
+
+} // namespace
+
+int main() {
+    auto success = true;
+
+    if (!test_success()) {
+        success = false;
+        std::println(std::cerr, "test_success failed");
+    }
+
+    if (!test_failure()) {
+        success = false;
+        std::println(std::cerr, "test_failure failed");
+    }
+
+    if (!test_consume_failure()) {
+        success = false;
+        std::println(std::cerr, "test_consume_failure failed");
+    }
+
+    if (!success) {
+        return 1;
+    }
+
+    return 0;
+}
