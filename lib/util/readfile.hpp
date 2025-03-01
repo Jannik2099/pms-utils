2c842c12eb20226541fec5bd777f06f8fe7f781e
John Turner <jturner.usa@gmail.com>
2025-03-01 15:08:40 -0500

    create try_readfile utility function

---
+++ lib/util/readfile.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <optional>
+#include <string>
+
+namespace [[gnu::visibility("default")]] pms_utils {
+namespace misc::_internal {
+
+inline std::optional<std::string> try_readfile(const std::filesystem::path &path) {
+
+    std::ifstream stream{path, std::ios::binary};
+
+    if (!stream.is_open()) {
+        return std::nullopt;
+    }
+
+    return std::string{std::istreambuf_iterator<char>{stream}, std::istreambuf_iterator<char>{}};
+};
+
+} // namespace misc::_internal
+} // namespace pms_utils
---
+++ test/misc/try_readfile.cpp
@@ -0,0 +1,47 @@
+#include "../lib/util/readfile.hpp"
+
+#include <filesystem>
+#include <iostream>
+#include <ostream>
+
+using pms_utils::misc::_internal::try_readfile;
+
+namespace {
+
+bool read_non_existant_file() {
+    const std::filesystem::path none{"/a/b/c/d"};
+
+    const auto readfile_result = try_readfile(none);
+
+    return !readfile_result.has_value();
+}
+
+bool read_file() {
+    const std::filesystem::path comm{"/proc/self/cmdline"};
+
+    const auto readfile_result = try_readfile(comm);
+
+    return readfile_result.value().contains("try_readfile");
+}
+
+} // namespace
+
+int main() {
+    auto success = true;
+
+    if (!read_non_existant_file()) {
+        success = false;
+        std::println(std::cerr, "read_non_existant_file failed");
+    }
+
+    if (!read_file()) {
+        success = false;
+        std::println(std::cerr, "read_file failed");
+    }
+
+    if (!success) {
+        return 1;
+    }
+
+    return 0;
+}
