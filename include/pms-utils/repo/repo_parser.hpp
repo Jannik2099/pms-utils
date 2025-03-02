c952423d1bb27eaa0fcc7a511710726024461695
Jannik Glückert <jannik.glueckert@gmail.com>
2025-03-02 16:17:01 +0100

    add repo_name parser

---
+++ include/pms-utils/repo/repo_parser.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "pms-utils/atom/atom_parser.hpp"
+#include "pms-utils/misc/x3_utils.hpp"
+
+#include <boost/parser/parser.hpp>
+#include <utility>
+
+namespace pms_utils::parsers::repo {
+
+namespace _internal {
+
+constexpr inline auto name_verifier = [](auto &ctx) {
+    std::string &attr = boost::parser::_attr(ctx);
+    std::string &val = boost::parser::_val(ctx);
+
+    if (attr.contains('+')) {
+        boost::parser::_pass(ctx) = false;
+        return;
+    }
+
+    val = std::move(attr);
+};
+
+} // namespace _internal
+
+PARSER_RULE_T(repo_name, std::string);
+
+PARSER_DEFINE(repo_name, parsers::atom::name[_internal::name_verifier]);
+
+} // namespace pms_utils::parsers::repo
