88ec367a3d0616ae306377e173b63c60a83de400
Jannik Glückert <jannik.glueckert@gmail.com>
2025-03-23 20:38:51 +0100

    fix #63

--- test/atom/name_like_ver.cpp
+++ test/atom/name_like_ver.cpp
@@ -11,6 +11,14 @@ int main() {
     ret &= try_parse("font-adobe-100dpi", parsers::atom::name).as_expected;
     ret &= try_parse("font-adobe-100dpi-1", parsers::atom::name, true, false).as_expected;
 
+    ret &= try_parse("foo-1", parsers::atom::name, true, false).as_expected;
+    ret &= try_parse("foo/bar-1-r1", parsers::atom::atom).as_expected;
+    ret &= try_parse("foo/bar-1-1-r1", parsers::atom::atom, true, false).as_expected;
+    ret &= try_parse("foo/bar-a1-1", parsers::atom::atom).as_expected;
+    ret &= try_parse("foo/bar-1a1-1", parsers::atom::atom).as_expected;
+    ret &= try_parse("foo/bar-1a-1", parsers::atom::atom, true, false).as_expected;
+    ret &= try_parse("foo/bar-1-bar", parsers::atom::atom).as_expected;
+
     if (!ret) {
         return 1;
     }
